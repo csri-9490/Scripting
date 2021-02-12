ac6296d44bd335521abc5c7e0a7ee2541ff13ba2
csri <srikanthchelukala@gmail.com>
2021-02-12 10:41:27 +0530

    s123

--- roboshops/components/common.sh
+++ roboshops/components/common.sh
@@ -4,5 +4,5 @@ USER_ID=$(id -u)
 
 if [ "$USER_ID" -ne 1 ]; then
   echo -e "\e[31myou must root user\e[0m"
-  exit 1
+  #exit 1 #if we want to stop there only, don't want got o cart and catalogue
 fi
