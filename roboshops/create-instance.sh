c4c764c2e9ea5bfe7953dcba6c35c2eeacbba4d0
csri <srikanthchelukala@gmail.com>
2021-02-14 15:32:22 +0530

    test

--- roboshops/create-instance.sh
+++ roboshops/create-instance.sh
@@ -6,7 +6,7 @@ if [ -z "${component}" ]; then
 fi
 STATE=$(aws ec2 describe-instances  --filters "Name=tag:Name,Values=${component}"  --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-if [ STATE == "running" ];then
+if [ $STATE == "running" ];then
   aws ec2 run-instances  --launch-template LaunchTemplateId=lt-06383d1526d457148 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 IpAddress=$(aws ec2 describe-instances  --filters "Name=tag:Name,Values=${component}"  --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
@@ -14,4 +14,4 @@ export component
 export IpAddress
 envsubst <record.json>/tmp/${component}.json
 
-#aws route53 change-resource-record-sets --hosted-zone-id Z0438602WZKT8OQ2PFB4 --change-batch file:///tmp/${component}.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0438602WZKT8OQ2PFB4 --change-batch file:///tmp/${component}.json
