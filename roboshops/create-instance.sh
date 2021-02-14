8e1cb69cfa13ad8ce178ead7297a39450d3791c5
csri <srikanthchelukala@gmail.com>
2021-02-14 16:14:47 +0530

    test

--- roboshops/create-instance.sh
+++ roboshops/create-instance.sh
@@ -6,8 +6,9 @@ if [ -z "${component}" ]; then
 fi
 STATE=$(aws ec2 describe-instances  --filters "Name=tag:Name,Values=${component}"  --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-if [ $STATE == "running" ];then
+if [ "$STATE" == "running" ];then
   aws ec2 run-instances  --launch-template LaunchTemplateId=lt-06383d1526d457148 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+  sleep 15
 fi
 IpAddress=$(aws ec2 describe-instances  --filters "Name=tag:Name,Values=${component}"  --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
