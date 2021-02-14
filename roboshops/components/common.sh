230953d77472a16a9eaf13988cb7b041bc8322ef
csri <srikanthchelukala@gmail.com>
2021-02-14 20:01:27 +0530

    test

--- roboshops/components/common.sh
+++ roboshops/components/common.sh
@@ -2,15 +2,16 @@
 #echo iam common file executing
 USER_ID=$(id -u)
 
-if [ "$USER_ID" -ne 1 ]; then
+if [ "$USER_ID" -ne 0 ]; then
   echo -e "\e[1;31myou must root user\e[0m"
   #exit 1 #if we want to stop there only, don't want got o cart and catalogue
+  exit 1
 fi
 #Linux os based
-Print(){
-  echo -e "\e[1m $(date +%c) \e[35m$(hostname) \e[0m \e[1;36m${COMPONENT}\e[0m::$1"
+#Print(){
+ # echo -e "\e[1m $(date +%c) \e[35m$(hostname) \e[0m \e[1;36m${COMPONENT}\e[0m::$1"
   #echo -e "\e[1m $(date +%c) \e[35m$(hostname) \e[0m \e[1;36m${COMPONENT}::$1"
-}
+#}
 #Maven based
 Print(){
 echo -e "[\e[1;34mINFO\e[0m]---------------------< \e[1m $1\e[0m >-----------"
@@ -26,6 +27,6 @@ Stat(){
     exit 2
 
   fi
-echo -e "[\e[1;34mINFO\e[0m]-----------------------------------------------"
+echo -e "[\e[1;34mINFO\e[0m]-----------------------------------------------\n"
 
 }
