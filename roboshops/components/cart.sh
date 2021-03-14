1a1b6ac7bcb30ab85ef027bed4497a4cb7a0638b
csri-9490 <78611228+csri-9490@users.noreply.github.com>
2021-03-14 13:07:44 +0530

    Update cart.sh

--- roboshops/components/cart.sh
+++ roboshops/components/cart.sh
@@ -12,9 +12,9 @@ Print "Adding RoboShop Project User" "useradd roboshop"
 id roboshop || useradd roboshop
 Stat $?
 
-Print "Download Cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/f62a488c-687f-4caf-9e5e-e751cf9b1603/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
+Print "Download Cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
 
-curl -s -L -o /tmp/cart.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/f62a488c-687f-4caf-9e5e-e751cf9b1603/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 Stat $?
 
